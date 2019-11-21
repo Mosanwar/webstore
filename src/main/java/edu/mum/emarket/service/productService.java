87f3dcdf7d88ae0ec10716437cf607544a05b6eb
HP <ibrahimkassem1092@gmail.com>
2019-11-21 01:00:45 -0600

    save changes

--- src/main/java/edu/mum/emarket/service/productService.java
+++ src/main/java/edu/mum/emarket/service/productService.java
@@ -4,19 +4,17 @@ package edu.mum.emarket.service;
 import edu.mum.emarket.domain.Product;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public interface ProductService {
 
 	List<Product> getAllProducts();
 
-	Product getProductById(long productID);
-	
-	Product getProductByProductId(String productID);
+	Product getProductById(String productID);
 
 	List<Product> getProductsByCategory(String category);
 
 
-	Product addProduct(Product product);
-	
-	Product editProduct(Product p);
+	void addProduct(Product product);
 }
